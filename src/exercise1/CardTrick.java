f97bae8d444d8786ab98d0840bcb5f42be085c58
Rasal Kareem <122823541+Rasalk@users.noreply.github.com>
2023-01-26 02:23:47 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -45,19 +45,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Rasal Kareem");
+        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
         System.out.println();
         
-        System.out.println("My career ambitions: To be a programmer with good pace");
+        System.out.println("My career ambitions:");
         System.out.println("-- Be more active on LinkedIn");
         System.out.println("-- Have a semester with no violations of academic integrity!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- football");
+        System.out.println("-- Investing");
         System.out.println("-- Cooking");
-        System.out.println("-- Reading/music");
-        System.out.println("-- Riding Vehicles");
+        System.out.println("-- Reading/Watching TV");
+        System.out.println("-- Riding my motorcycle");
 
         System.out.println();
         
